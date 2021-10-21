7d71564849cd6d44d6f41ec4e7c274c0dc90ff0c
ayozzet <ayozzet@gmail.com>
2021-10-21 18:12:41 +0800

    count frame edit

--- Video_CountFrame.py
+++ Video_CountFrame.py
@@ -1,6 +1,8 @@
 import cv2
 
 cap= cv2.VideoCapture('videoplayback(360p).mp4')
+#cap= cv2.VideoCapture('videoplayback(720p).mp4')
+#cap= cv2.VideoCapture('Python (OpenCV) Course 01 - Read and Display Image.mp4')
 
 totalframecount= int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
